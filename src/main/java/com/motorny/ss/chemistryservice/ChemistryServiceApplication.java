9870cee96d21591d7f578a647c09ca918997159e
Sergey Motorny <motorny0919@gmail.com>
2024-05-09 18:33:35 +0300

    creating a project

---
+++ src/main/java/com/motorny/ss/chemistryservice/ChemistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.motorny.ss.chemistryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemistryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChemistryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/motorny/ss/chemistryservice/ChemistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorny.ss.chemistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
