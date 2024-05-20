c1c0d830e4eb8b64a0b2f7e1280ede11aaaa7f2b
Sergey Motorny <motorny0919@gmail.com>
2024-05-20 20:47:51 +0300

    fix n+1 problem

--- src/main/java/com/motorny/ss/chemistryservice/repository/UserRepository.java
+++ src/main/java/com/motorny/ss/chemistryservice/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.motorny.ss.chemistryservice.repository;
 
 import com.motorny.ss.chemistryservice.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-//    @Query(value = "FROM User user JOIN FETCH user.reviews")
-//    List<User> findAll();
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = "reviews")
+    List<User> findAll();
 }
